4f3cfde76bc53874a82a77820793978ac3d90340
Toru <t.furuya825@gmail.com>
2019-07-18 09:54:06 +0900

    Initial commit

---
+++ TabAndAnimation/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabAndAnimation
+//
+//  Created by Toru Furuya on 2019/07/18.
+//  Copyright © 2019 Toru Furuya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabAndAnimation/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabAndAnimation
+//
+//  Created by Toru Furuya on 2019/07/18.
+//  Copyright © 2019 Toru Furuya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
