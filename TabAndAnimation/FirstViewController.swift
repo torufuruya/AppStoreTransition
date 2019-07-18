0a8671123b77cbe91dfa5f0cf7cfcbca4594d51a
Toru <t.furuya825@gmail.com>
2019-07-18 17:59:27 +0900

    DismissCardAnimator

--- TabAndAnimation/FirstViewController.swift
+++ TabAndAnimation/FirstViewController.swift
@@ -109,6 +109,15 @@ final class CardTransition: NSObject, UIViewControllerTransitioningDelegate {
         )
         return PresentCardAnimator(params: params)
     }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        let params = DismissCardAnimator.Params.init(
+            fromCardFrame: self.params.fromCardFrame,
+            fromCardFrameWithoutTransform: self.params.fromCardFrameWithoutTransform,
+            fromCell: self.params.fromCell
+        )
+        return DismissCardAnimator(params: params)
+    }
 }
 
 final class PresentCardAnimator: NSObject, UIViewControllerAnimatedTransitioning {
@@ -284,5 +293,113 @@ final class PresentCardTransitionDriver {
         }
 
         self.animator = baseAnimator
+    }
+}
+
+final class DismissCardAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+
+    struct Params {
+        let fromCardFrame: CGRect
+        let fromCardFrameWithoutTransform: CGRect
+        let fromCell: CardCollectionViewCell
+    }
+
+    struct Constants {
+        static let relativeDurationBeforeNonInteractive: TimeInterval = 0.5
+        static let minimumScaleBeforeNonInteractive: CGFloat = 0.8
+    }
+
+    private let params: Params
+
+    init(params: Params) {
+        self.params = params
+        super.init()
+    }
+
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.6
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let ctx = transitionContext
+        let container = ctx.containerView
+        let screens: (cardDetail: DetailViewController, home: UITabBarController) = (
+            ctx.viewController(forKey: .from)! as! DetailViewController,
+            ctx.viewController(forKey: .to)! as! UITabBarController
+        )
+
+        let cardDetailView = ctx.view(forKey: .from)!
+
+        let animatedContainerView = UIView()
+        animatedContainerView.translatesAutoresizingMaskIntoConstraints = false
+        cardDetailView.translatesAutoresizingMaskIntoConstraints = false
+
+        container.removeConstraints(container.constraints)
+
+        container.addSubview(animatedContainerView)
+        animatedContainerView.addSubview(cardDetailView)
+
+        // Card fills inside animated container view
+        cardDetailView.edges(to: animatedContainerView)
+
+        animatedContainerView.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
+        let animatedContainerTopConstraint = animatedContainerView.topAnchor.constraint(equalTo: container.topAnchor, constant: 0)
+        let animatedContainerWidthConstraint = animatedContainerView.widthAnchor.constraint(equalToConstant: cardDetailView.frame.width)
+        let animatedContainerHeightConstraint = animatedContainerView.heightAnchor.constraint(equalToConstant: cardDetailView.frame.height)
+
+        NSLayoutConstraint.activate([animatedContainerTopConstraint, animatedContainerWidthConstraint, animatedContainerHeightConstraint])
+
+        // Fix weird top inset
+        let topTemporaryFix = screens.cardDetail.cardContentView.topAnchor.constraint(equalTo: cardDetailView.topAnchor)
+        topTemporaryFix.isActive = true
+
+        container.layoutIfNeeded()
+
+        // Force card filling bottom
+        let stretchCardToFillBottom = screens.cardDetail.cardContentView.bottomAnchor.constraint(equalTo: cardDetailView.bottomAnchor)
+
+        func animateCardViewBackToPlace() {
+            stretchCardToFillBottom.isActive = true
+            // Back to identity
+            // NOTE: Animated container view in a way, helps us to not messing up `transform` with `AutoLayout` animation.
+            cardDetailView.transform = .identity
+            animatedContainerTopConstraint.constant = self.params.fromCardFrameWithoutTransform.minY
+            animatedContainerWidthConstraint.constant = self.params.fromCardFrameWithoutTransform.width
+            animatedContainerHeightConstraint.constant = self.params.fromCardFrameWithoutTransform.height
+            container.layoutIfNeeded()
+        }
+
+        func completeEverything() {
+            let success = !ctx.transitionWasCancelled
+            animatedContainerView.removeConstraints(animatedContainerView.constraints)
+            animatedContainerView.removeFromSuperview()
+            if success {
+                cardDetailView.removeFromSuperview()
+                self.params.fromCell.isHidden = false
+            } else {
+                // Remove temporary fixes if not success!
+                topTemporaryFix.isActive = false
+                stretchCardToFillBottom.isActive = false
+
+                cardDetailView.removeConstraint(topTemporaryFix)
+                cardDetailView.removeConstraint(stretchCardToFillBottom)
+
+                container.removeConstraints(container.constraints)
+
+                container.addSubview(cardDetailView)
+                cardDetailView.edges(to: container)
+            }
+            ctx.completeTransition(success)
+        }
+
+        UIView.animate(withDuration: transitionDuration(using: ctx), delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: [], animations: {
+            animateCardViewBackToPlace()
+        }) { (finished) in
+            completeEverything()
+        }
+
+        UIView.animate(withDuration: transitionDuration(using: ctx) * 0.6) {
+            screens.cardDetail.scrollView.contentOffset = .zero
+        }
     }
 }
