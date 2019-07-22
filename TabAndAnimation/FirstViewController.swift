e975d77e6883f41b087ebe31370fddd0332531b5
Toru <t.furuya825@gmail.com>
2019-07-22 16:25:15 +0900

    Small card collection in first

--- TabAndAnimation/FirstViewController.swift
+++ TabAndAnimation/FirstViewController.swift
@@ -47,17 +47,14 @@ class FirstViewController: UIViewController {
         CardContentViewModel(image: #imageLiteral(resourceName: "image1"), primary: "Card 3")
     ]
 
-    private var viewModelsForSmall = [
-        CardContentViewModel(image: #imageLiteral(resourceName: "image1"), primary: "Card 1"),
-        CardContentViewModel(image: #imageLiteral(resourceName: "image2"), primary: "Card 2"),
-        CardContentViewModel(image: #imageLiteral(resourceName: "image1"), primary: "Card 3"),
-        CardContentViewModel(image: #imageLiteral(resourceName: "image2"), primary: "Card 4"),
-        CardContentViewModel(image: #imageLiteral(resourceName: "image1"), primary: "Card 5")
-    ]
+    private let smallCardCollection = SmallCardCollection()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //----------------
+        // Card Collection View
+        //----------------
         self.collectionView.delaysContentTouches = false
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
@@ -66,14 +63,24 @@ class FirstViewController: UIViewController {
 
         let cardLayout = CardCollectionViewFlowLayout()
         cardLayout.scrollDirection = .horizontal
-        cardLayout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        cardLayout.sectionInset = UIEdgeInsets(top: 0, left: 32, bottom: 0, right: 32)
         self.collectionView.collectionViewLayout = cardLayout
+
+        //----------------
+        // Small Card Collection View
+        //----------------
+        self.collectionViewForSmall.delaysContentTouches = false
+        self.smallCardCollection.viewController = self
+        self.collectionViewForSmall.delegate = smallCardCollection
+        self.collectionViewForSmall.dataSource = smallCardCollection
+        self.collectionViewForSmall.clipsToBounds = false
+        self.collectionViewForSmall.register(UINib(nibName: "\(CardCollectionViewCell.self)", bundle: nil), forCellWithReuseIdentifier: "SmallCard")
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         if let cardLayout = self.collectionView.collectionViewLayout as? CardCollectionViewFlowLayout {
-            let cardHorizontalOffset: CGFloat = 20
+            let cardHorizontalOffset = cardLayout.sectionInset.left
             let cardHeightByWidthRatio: CGFloat = 0.6
             let width = collectionView.bounds.size.width - 2 * cardHorizontalOffset
             let height: CGFloat = width * cardHeightByWidthRatio
@@ -142,5 +149,81 @@ extension FirstViewController: UICollectionViewDelegateFlowLayout {
         detail.viewModel = self.viewModels[indexPath.row]
 
         self.present(detail, animated: true)
+    }
+}
+
+class SmallCardCollection: NSObject, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    var viewController: UIViewController?
+
+    private var transition: SmallCardTransition?
+
+    private var viewModels = [
+        CardContentViewModel(image: #imageLiteral(resourceName: "image1"), primary: "Card 1"),
+        CardContentViewModel(image: #imageLiteral(resourceName: "image2"), primary: "Card 2"),
+        CardContentViewModel(image: #imageLiteral(resourceName: "image1"), primary: "Card 3"),
+        CardContentViewModel(image: #imageLiteral(resourceName: "image2"), primary: "Card 4"),
+        CardContentViewModel(image: #imageLiteral(resourceName: "image1"), primary: "Card 5")
+    ]
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SmallCard", for: indexPath) as! CardCollectionViewCell
+        cell.cardContentView.viewModel = self.viewModels[indexPath.row]
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SmallCard", for: indexPath) as! CardCollectionViewCell
+        cell.cardContentView.viewModel = self.viewModels[indexPath.row]
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: 150)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detail = DetailViewController()
+
+        // Get tapped cell location
+        let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
+        // Get current frame on screen
+        let currentCellFrame = cell.layer.presentation()!.frame
+        // Convert current frame to screen's coordinates
+        let cardPresentationFrameOnScreen = cell.superview!.convert(currentCellFrame, to: nil)
+        // Get card frame without transform in screen's coordinates  (for the dismissing back later to original location)
+        let cardFrameWithoutTransform = { () -> CGRect in
+            let center = cell.center
+            let size = cell.bounds.size
+            let r = CGRect(
+                x: center.x - size.width / 2,
+                y: center.y - size.height / 2,
+                width: size.width,
+                height: size.height
+            )
+            return cell.superview!.convert(r, to: nil)
+        }()
+
+        let params = SmallCardTransition.Params(
+            fromCardFrame: cardPresentationFrameOnScreen,
+            fromCardFrameWithoutTransform: cardFrameWithoutTransform,
+            fromCell: cell)
+        self.transition = SmallCardTransition(params: params)
+        detail.transitioningDelegate = self.transition
+
+        // If `modalPresentationStyle` is not `.fullScreen`, this should be set to true to make status bar depends on presented vc.
+        detail.modalPresentationCapturesStatusBarAppearance = true
+        detail.modalPresentationStyle = .custom
+
+        detail.viewModel = self.viewModels[indexPath.row]
+
+        self.viewController?.present(detail, animated: true)
     }
 }
